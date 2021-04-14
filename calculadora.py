68427c4b5827796a4f0a633cfffb5859d073e71c
Posccis <lkzq33@gmail.com>
2021-04-13 21:48:16 -0300

    Programa em python com calculadora que utiliza apenas o sucessor(+1)

--- calculadora.py
+++ calculadora.py
@@ -1,12 +1,16 @@
+""" FUNÇÃO SUCESSOR """
+def Suc(valor_a):
+    return valor_a + 1
+
 
 """ FUNÇÃO PARA MULTIPLICAR (multiply function) """
 def Mult(value_a, value_b):
-    print(value_a, value_b)
+    
     resultado = 0
     for j in range(value_b):
         for i in range(value_a):
             resultado += 1
-            print(resultado)
+          
         
     return resultado
 """---FIM DA FUNÇÃO (end of the function)---"""
@@ -32,7 +36,40 @@ def Exp(value_a, value_b):
 
 """ --- ENTRADAS (INPUTS) --- """
 
-ope, valor_a, valor_b = input("bota: ").split()
+ope = "stringo"
+results = []
+
+while ope != "":
+    try:
+        entrada = input().split()
+        ope = str(entrada[0])
+    except:
+        ope = ""
+       
+    if ope == "Mult":
+        valor_a, valor_b = entrada[1], entrada[2]
+        results.append(Mult(int(valor_a), int(valor_b)))
+    elif ope == "Soma":
+        valor_a, valor_b = entrada[1], entrada[2]
+        results.append(Soma(int(valor_a), int(valor_b)))
+    elif ope == "Suc":
+
+        valor_a = entrada[1]
+        results.append(Suc(int(valor_a)))
+    elif ope == "Exp":
+        valor_a, valor_b = entrada[1], entrada[2]
+        results.append(Exp(int(valor_a), int(valor_b)))
+    else:
+        ope = ""
+        
+""" --- FIM DAS ENTRADAS (end of the inputs) ---"""
+
+""" SAIDAS (outputs) """
+
+for i in range(len(results)):
+
+    print(results[i])
+
+""" --- FIM DAS SAIDAS --- """
 
 
-print(Exp(int(valor_a), int(valor_b)))
